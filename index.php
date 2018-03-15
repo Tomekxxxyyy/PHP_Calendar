7280b7376e7a99ef456fc34df584b92fc016553d
Tomasz Wieckiewicz <tomasz.wieckiewicz@eastside-solutions.com>
2018-03-15 10:45:03 +0100

    styczenxxx

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     }
     $start = mktime(12, 0, 0, $miesiac, 1, $rok);
     $tablicaPierwszyDzien = getdate($start);
-    $miesiace = array("styczeń", "luty", "marzec", "kwiecień", "maj", "czerwiec", "lipiec",
+    $miesiace = array("styczeńxxx", "luty", "marzec", "kwiecień", "maj", "czerwiec", "lipiec",
         "sierpień", "wrzesień", "pażdziernik", "listopad", "grudzień");
 ?>
 <!DOCTYPE html>
